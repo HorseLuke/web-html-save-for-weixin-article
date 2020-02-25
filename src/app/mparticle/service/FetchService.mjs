4f98be03e3b1ab8e783c9e8ce1d9ee7abd828c72
horseluke <horseluke@126.com>
2020-02-25 23:14:23 +0800

    修改：evaluateHandle移动到浏览器脚本注入

--- src/app/mparticle/service/FetchService.mjs
+++ src/app/mparticle/service/FetchService.mjs
@@ -50,65 +50,30 @@ class FetchService{
             const bodyHandle = await page.$wait("body");
 
             await page.addScriptTag({
-                content: String(readFileSync(global.BootstarpInstance.getAppdir() + "/browser/script/BaseHelper.js")),
+                content: String(readFileSync(global.BootstarpInstance.getAppdir() + "/browser/script/base/BaseHelper.js")),
+            });
+
+            await page.addScriptTag({
+                content: String(readFileSync(global.BootstarpInstance.getAppdir() + "/browser/script/mparticle/FetchServiceEvaluateHandleHelper.js")),
             });
 
             const navigatorJSHandle = await page.evaluateHandle(() => {
-                return {
-                    userAgent: navigator.userAgent
-                };
+                return window.FetchServiceEvaluateHandleHelper.getUserAgent();
             });
     
             console.log(await navigatorJSHandle.jsonValue());
 
             const imgJSHandle = await page.evaluateHandle(() => {
-                let imgListResult = [];
-                let list = document.querySelectorAll(".rich_media_content img");
-                if(list.length < 1){
-                    return imgListResult;
-                }
-
-                for(let i=0; i < list.length; i++){
-                    let imgObj = list[i];
-                    let src = imgObj.getAttribute("data-src");
-                    if(window.xHelperFunctionInBrowserContext.checkUrlIsInHostList(src, ["mmbiz.qpic.cn"]) != 0){
-                        continue;
-                    }
-                    let randomstr = (i + 1) + "-" + window.xHelperFunctionInBrowserContext.generateRandomStr(16);
-                    imgObj.setAttribute("data-x-replace-token", randomstr);
-                    imgObj.setAttribute("data-x-src-origin", src);
-                    imgObj.setAttribute("data-x-need-replace-download", 1);
-                    imgListResult.push({
-                        "data-x-replace-token": randomstr,
-                        "data-x-src-origin": src
-                    });
-                }
-
-                return imgListResult;
-
+                return window.FetchServiceEvaluateHandleHelper.getMpImgList();
             });
 
             console.log(await imgJSHandle.jsonValue());
             
             const pageHTMLJSHandler = await page.evaluateHandle(() => {
-                let title = document.querySelector(".rich_media_title").innerText;
-                try{
-                    document.querySelector("#js_profile_qrcode").style.display = "block";
-                }catch(e){
-                    console.error(e);
-                }
-                let authorHtml = document.querySelector(".rich_media_meta_list").innerHTML;
-                let contentHtml = document.querySelector(".rich_media_content").innerHTML;
-
-                return {
-                    title: title,
-                    authorHtml: authorHtml,
-                    contentHtml: contentHtml
-                };
-
+                return window.FetchServiceEvaluateHandleHelper.getMainHTML();
             });
 
-            //console.log(await pageHTMLJSHandler.jsonValue());
+            console.log(await pageHTMLJSHandler.jsonValue());
 
     
         }catch(e){
---
+++ src/browser/script/mparticle/FetchServiceEvaluateHandleHelper.js
@@ -0,0 +1,56 @@
+(function(name){
+
+    let helper = {};
+
+    helper.getUserAgent = function(){
+        return {
+            userAgent: navigator.userAgent
+        };
+    };
+
+    helper.getMpImgList = function(){
+        let imgListResult = [];
+        let list = document.querySelectorAll(".rich_media_content img");
+        if(list.length < 1){
+            return imgListResult;
+        }
+
+        for(let i=0; i < list.length; i++){
+            let imgObj = list[i];
+            let src = imgObj.getAttribute("data-src");
+            if(window.xHelperFunctionInBrowserContext.checkUrlIsInHostList(src, ["mmbiz.qpic.cn"]) != 0){
+                continue;
+            }
+            let randomstr = (i + 1) + "-" + window.xHelperFunctionInBrowserContext.generateRandomStr(16);
+            imgObj.setAttribute("data-x-replace-token", randomstr);
+            imgObj.setAttribute("data-x-src-origin", src);
+            imgObj.setAttribute("data-x-need-replace-download", 1);
+            imgListResult.push({
+                "data-x-replace-token": randomstr,
+                "data-x-src-origin": src
+            });
+        }
+
+        return imgListResult;
+    };
+
+    helper.getMainHTML = function(){
+        let title = document.querySelector(".rich_media_title").innerText;
+        try{
+            document.querySelector("#js_profile_qrcode").style.display = "block";
+        }catch(e){
+            console.error(e);
+        }
+        let authorHtml = document.querySelector(".rich_media_meta_list").innerHTML;
+        let contentHtml = document.querySelector(".rich_media_content").innerHTML;
+
+        return {
+            title: title,
+            authorHtml: authorHtml,
+            contentHtml: contentHtml
+        };
+    };
+
+    window[name] = helper;
+
+})("FetchServiceEvaluateHandleHelper");
