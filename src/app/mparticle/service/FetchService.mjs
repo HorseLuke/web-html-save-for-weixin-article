893520ede1bba6ff8b709d5af1c669d5bddb4c29
horseluke <horseluke@126.com>
2020-02-26 00:07:10 +0800

    增加：下载

--- src/app/mparticle/service/FetchService.mjs
+++ src/app/mparticle/service/FetchService.mjs
@@ -1,8 +1,9 @@
 import {readFileSync} from "fs";
 import randomstring from "randomstring";
+import config from "config";
 import BrowserContainerService from "../../playwright/service/BrowserContainerService.mjs";
 import {checkUrlIsInHostList} from "../../base/helper/UrlHelper.mjs";
-
+import ImageDownloadService from "../../download/service/ImageDownloadService.mjs";
 
 
 class FetchService{
@@ -19,10 +20,12 @@ class FetchService{
         const currentTime = new Date();
 
         const articleMeta = {
-            id: "",
+            id: "1900-00-00-test0000",
             url: url,
             save_timestamp: 0,
-            save_date: "1900-00-00"
+            save_date: "1900-00-00",
+            save_timecode: "000000",
+            save_dir: "",
         };
 
         articleMeta.save_timestamp = parseInt(currentTime.getTime() / 1000);
@@ -31,7 +34,13 @@ class FetchService{
             ('0'+ (currentTime.getMonth() + 1)).slice(-2),
             ('0'+ currentTime.getDate()).slice(-2)
         ].join("-");
-        articleMeta.id = articleMeta.save_date + "-" + randomstring.generate(16);
+        articleMeta.save_timecode = [
+            ('0'+ currentTime.getHours()).slice(-2),
+            ('0'+ currentTime.getMinutes()).slice(-2),
+            ('0'+ currentTime.getSeconds()).slice(-2)
+        ].join("");
+        articleMeta.id = randomstring.generate(32);
+        articleMeta.save_dir = config.get("mpArticleSave.dir_path") + "/" + articleMeta.save_date + "/" + articleMeta.save_timecode + "_" +  articleMeta.id;
 
         //初始化browser
         const browserContainerInstance = await BrowserContainerService.createInstanceByConfigName("chromium", "playwrightChromiumLaunchDefault");
@@ -54,26 +63,32 @@ class FetchService{
             });
 
             await page.addScriptTag({
-                content: String(readFileSync(global.BootstarpInstance.getAppdir() + "/browser/script/mparticle/FetchServiceEvaluateHandleHelper.js")),
+                content: String(readFileSync(global.BootstarpInstance.getAppdir() + "/browser/script/mparticle/FetchServiceMpArticleEvaluateHandleHelper.js")),
             });
 
-            const navigatorJSHandle = await page.evaluateHandle(() => {
-                return window.FetchServiceEvaluateHandleHelper.getUserAgent();
+            const navigatorJSHandlePromise = page.evaluateHandle(() => {
+                return window.FetchServiceMpArticleEvaluateHandleHelper.getUserAgent();
             });
     
-            console.log(await navigatorJSHandle.jsonValue());
-
-            const imgJSHandle = await page.evaluateHandle(() => {
-                return window.FetchServiceEvaluateHandleHelper.getMpImgList();
+            const imgJSHandlePromise = page.evaluateHandle(() => {
+                return window.FetchServiceMpArticleEvaluateHandleHelper.getMpImgList();
             });
 
-            console.log(await imgJSHandle.jsonValue());
+            const pageHTMLJSHandlePromise = await page.evaluateHandle(() => {
+                return window.FetchServiceMpArticleEvaluateHandleHelper.getMainHTML();
+            });
             
-            const pageHTMLJSHandler = await page.evaluateHandle(() => {
-                return window.FetchServiceEvaluateHandleHelper.getMainHTML();
-            });
+            const htmlEvaluateResult = await Promise.all([
+                navigatorJSHandlePromise,
+                imgJSHandlePromise,
+                pageHTMLJSHandlePromise
+            ]);
 
-            console.log(await pageHTMLJSHandler.jsonValue());
+            const imageListDownloaded = await this._downloadImageFromEvaluateResult(articleMeta, htmlEvaluateResult[0], htmlEvaluateResult[1]);
+
+            console.log(imageListDownloaded);
+
+            //await page.goto("file://" + global.BootstarpInstance.getAppdir() + "/browser/html/MpArticleBlank.html");
 
     
         }catch(e){
@@ -88,6 +103,48 @@ class FetchService{
         await browserContainerInstance.close();
 
         return 0;
+
+    }
+
+
+    async _downloadImageFromEvaluateResult(articleMeta, navigatorJSHandle, imgJSHandle){
+
+        const navigatorInfo = await navigatorJSHandle.jsonValue();
+
+        const imageDownloadDefaultOptions = {
+            headers: {
+                Referer: articleMeta.url,
+                "user-agent": navigatorInfo.userAgent
+            },
+            save: {
+                dir_path: articleMeta.save_dir + "/images/",
+                url_prefix: "./images/",
+            }
+        };
+
+        const imageList = await imgJSHandle.jsonValue();
+
+        const imageNeedDownload = [];
+
+        for(let i=0; i < imageList.length; i++){
+            let row = imageList[i];
+            let newRow = {
+                url: row["data-x-src-origin"],
+                options: {
+                    "unique-id": row["data-x-replace-token"], 
+                    save: { 
+                        name: row["data-x-replace-token"]
+                    }
+                },
+            };
+
+            imageNeedDownload.push(newRow);
+        }
+
+        const ImageDownloadServiceInstance = new ImageDownloadService();
+
+        const result = await ImageDownloadServiceInstance.downloadImagelist(imageNeedDownload, imageDownloadDefaultOptions);
+        return result;
 
     }
 
