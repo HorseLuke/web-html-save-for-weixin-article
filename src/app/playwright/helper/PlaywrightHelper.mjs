bbc846a4fe0534ed74d917b00d452e39ff9b6842
horseluke <horseluke@126.com>
2020-03-01 00:33:36 +0800

    修改：PlaywrightHelper修改close函数，以更彻底释放无用内容

--- src/app/playwright/helper/PlaywrightHelper.mjs
+++ src/app/playwright/helper/PlaywrightHelper.mjs
@@ -11,11 +11,19 @@ class PlaywrightHelper{
     }
 
     static async createBrowserByConfig(browserType, launchOptions, limitRuntime){
+
         limitRuntime = limitRuntime || 0;
+
         const browser = await playwright.default[browserType].launch(launchOptions);
+        browser._originCloseMethod = browser.close;
+
         LimitRunTimerService.attachNewToInstance(browser);
-        browser.limitRunTimerServiceInstance.onReachEndtimeEvent = async () => {
-            await browser.close();
+        browser.limitRunTimerServiceInstance.onReachEndtimeEvent = async function(){
+            console.log("a browser is closing...");
+            await browser._originCloseMethod();
+        };
+        browser.close = async function(){
+            await browser.limitRunTimerServiceInstance.runReachEndtime();
         };
         browser.limitRunTimerServiceInstance.keepAliveTime = limitRuntime;
         return browser;
@@ -35,9 +43,15 @@ class PlaywrightHelper{
         }
 
         const context = await browserInstance.newContext(config);
+        context._originCloseMethod = context.close;
+
         LimitRunTimerService.attachNewToInstance(context);
-        context.limitRunTimerServiceInstance.onReachEndtimeEvent = async () => {
-            await context.close();
+        context.limitRunTimerServiceInstance.onReachEndtimeEvent = async function(){
+            console.log("a browser context is closing...");
+            await context._originCloseMethod();
+        };
+        context.close = async function(){
+            await context.limitRunTimerServiceInstance.runReachEndtime();
         };
         context.limitRunTimerServiceInstance.keepAliveTime = limitRuntime;
 
@@ -50,12 +64,17 @@ class PlaywrightHelper{
         limitRuntime = limitRuntime || 0;
 
         const page = await contextInstance.newPage();
+        page._originCloseMethod = page.close;
 
         LimitRunTimerService.attachNewToInstance(page);
-        page.limitRunTimerServiceInstance.onReachEndtimeEvent = async () => {
-            await page.close();
+        page.limitRunTimerServiceInstance.onReachEndtimeEvent = async function(){
+            console.log("a page is closing...");
+            await page._originCloseMethod();
         };
         page.limitRunTimerServiceInstance.keepAliveTime = limitRuntime;
+        page.close = async function(){
+            await page.limitRunTimerServiceInstance.runReachEndtime();
+        };
 
         if(url){
             await page.goto(url);
