d3f809ac55fc6a34b49251bf6bb96da306701010
horseluke <horseluke@126.com>
2020-03-02 20:15:13 +0800

    增加：UrlHelper注释

--- src/app/base/helper/UrlHelper.mjs
+++ src/app/base/helper/UrlHelper.mjs
@@ -1,5 +1,15 @@
 import {parse as urlParse} from "url";
 
+/**
+ * 检查某个url是否在指定的域名列表白名单中
+ * 
+ * @example
+ *     checkUrlIsInHostList("http://www.aaa.com/asdgg/dasfg", ["www.aaa.com", "www.bbb.com"]); //0
+ * 
+ * @param {string} urlParam url路径 
+ * @param {Array} whiteHostList 域名列表白名单
+ * @return {number} 0：检查通过；-1：检查不通过；-2：报错
+ */
 function checkUrlIsInHostList(urlParam, whiteHostList){
 
 	try{
