4fad629e74dda86d82ca7262a128e4e56c508275
horseluke <horseluke@126.com>
2020-03-15 22:45:21 +0800

    增加：yargs模块调用

--- src/app.mjs
+++ src/app.mjs
@@ -1,12 +1,29 @@
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-import util from 'util';
-//import moduleAlias from "module-alias";
 import BootstarpService from "./app/base/service/BootstarpService.mjs";
+import yargs from "yargs";
 
 if(!process.env.NODE_ENV){
     process.env.NODE_ENV = "production";
 }
+
+//https://nodejs.org/en/knowledge/command-line/how-to-parse-command-line-arguments/
+const argv = yargs
+    .option('ctype', {
+        description: 'Controller Type Name',
+        default: "cli",
+        type: 'string',
+    })
+    .option('router', {
+        alias: 'r',
+        description: 'Router Path',
+        default: "index/index/index",
+        type: 'string',
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
 
 //全局唯一启动模块
 /**
@@ -15,6 +32,18 @@ if(!process.env.NODE_ENV){
 BootstarpService.instance;    //启动唯一启动模块
 
 console.log("process.env.NODE_ENV is " + process.env.NODE_ENV);
+
+console.log(argv);
+
+if(!BootstarpService.instance.checkCtype(argv.ctype)){
+    throw new Error("ctype option not right!");
+}
+
+const routerResult = BootstarpService.instance.parseRouter(argv.router);
+if(routerResult.error != 0){
+    throw new Error("router option not right!");
+}
+
 
 //console.log(util.inspect(process.env, false, null, true /* enable colors */));
 
@@ -31,19 +60,6 @@ console.log("BootstarpService.instance.getAppdir() is " + BootstarpService.insta
 //初始化应用
 (async () => {
     const config = await import("config");
-
-    const testCtrl = await BootstarpService.instance.ESMImportFromAppdir("app/mparticle/testcontroller/FetchOneArticleTestController.mjs");
-    const testCtrlInstance = new testCtrl.default();
-    const result = await testCtrlInstance.testWithMultiPic();
-
-
-   /*
-   const testCtrl = await BootstarpService.instance.ESMImportFromAppdir("app/download/testcontroller/BatchImageDownloadTestController.mjs");
-   const testCtrlInstance = new testCtrl.default();
-   const result = await testCtrlInstance.testBatch();
-    */
-    console.log(result);
-
 
 })();
 
