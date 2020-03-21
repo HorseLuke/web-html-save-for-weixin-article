26a96d2766972c0900f7b6f4b1538ec591e6eac3
horseluke <horseluke@126.com>
2020-03-22 02:29:20 +0800

    增加：production配置

---
+++ src/config/production.js
@@ -0,0 +1,12 @@
+module.exports = {
+    playwrightChromiumLaunchDefault : {
+        executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    },
+    imageDownload: {
+        dir_path: __dirname + "/../../runtime/images",
+        url_prefix: "./images/",
+    },
+    mpArticleSave:{
+        dir_path: __dirname + "/../../runtime/mp-article"
+    }
+};
