0f71f485ed49d0ffcf9dbde97ab44900ce3b6c53
horseluke <horseluke@126.com>
2020-03-21 23:47:41 +0800

    增加：常量

---
+++ src/app/base/helper/Constants.mjs
@@ -0,0 +1,17 @@
+/**
+ * 常量定义
+ */
+
+/**
+  * @const int defined from the BSD Syslog message severities
+  * @link http://tools.ietf.org/html/rfc3164
+ */
+export const MSG_LEVEL_EMERG  = 0;    // Emergency: system is unusable
+export const MSG_LEVEL_ALERT  = 1;    // Alert: action must be taken immediately
+export const MSG_LEVEL_CRIT   = 2;    // Critical: critical conditions
+export const MSG_LEVEL_ERR    = 3;    // Error: error conditions
+export const MSG_LEVEL_WARN   = 4;    // Warning: warning conditions
+export const MSG_LEVEL_NOTICE = 5;    // Notice: normal but significant condition
+export const MSG_LEVEL_INFO   = 6;    // Informational: informational messages
+export const MSG_LEVEL_DEBUG  = 7;    // Debug: debug messages
+
